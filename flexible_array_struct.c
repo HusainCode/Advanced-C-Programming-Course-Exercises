1788bfece65ba899581f29918a0e1a95d649f994
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2024-02-26 21:41:41 -0600

    Add files via upload

--- flexible_array_struct.c
+++ flexible_array_struct.c
@@ -10,31 +10,59 @@
 2- Create a structure named MyArray that has a length member and a flexible
 array member named array.
 3- Read in from the user the size of the array at
-runtime. 4- Allocate memory for the structure based on this size read in from
-the user. 5- Set the length member and fill the array with some dummy data. 6-
-Print the array elements.
+runtime.
+4- Allocate memory for the structure based on this size read in from
+the user.
+5- Set the length member and fill the array with some dummy data.
+6-Print the array elements.
   */
 
 #include <malloc.h>
 #include <stdio.h>
+#include <stdlib.h> // For rand()
 #include <string.h>
-#include <malloc.h>
+#include <time.h> // For seeding the random number generator
 
+/*
+*A flexible array member can be declared only as the last member of a struct.
+*Each struct can contain at most one flexible array member.
+*A flexible array cannot be the only member of a struct.
+  .The struct must also have one or more fixed members.
+*Any struct containing a flexible array member can't be a member of another
+struct.
+*A struct with a flexible array member can't be statically initialized.
+  .It must be allocated dynamically.
+  .You can't fix the size of the flexible array member at compile time.
+
+
+*A struct with a flexible array member reduces the number of allocations for it
+by 1/2.
+.Instead of two allocations for one struct object, you need just 1.
+.You save the storage for one additional pointer.
+*/
+void fillArrayWithRandomNumbers(int *arr, size_t size);
 struct myArray {
     int length;
-    int arry[]; // the array must be the last member
-};
-
-void *malloc(size_t size);
+    int arry[];
+}; // end of struct
 
 int main() {
 
-    struct myArray myInstance;
+    // Avoid unassigned variable to prevent undefined behaviors.
+    size_t size = 0;
+    struct myArray *myInstance;
 
+    printf("Enter array size: ");
+    scanf("%d", &size);
 
-    printf("Enter the array size: ");
-    scanf("%d", &myInstance.length);
-
+    myInstance = malloc(sizeof(struct myArray) + size * sizeof(int));
 
     return 0;
+}
+
+// Fills the given array with random numbers
+void fillArrayWithRandomNumbers(int *arr, size_t size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 10; // Assign a random number (0-9) to each array element
+    }
 }
