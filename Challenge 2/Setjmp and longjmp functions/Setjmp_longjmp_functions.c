108482a2cc4e01d0c9470a3ab9bba672b8e81c40
Husain Alshaikahmed <alshaikhahmed@hotmail.com>
2024-03-15 05:09:50 -0500

    added code and the description 2nd challenge

--- Challenge 2/Setjmp and longjmp functions/Setjmp_longjmp_functions.c
+++ Challenge 2/Setjmp and longjmp functions/Setjmp_longjmp_functions.c
@@ -1,16 +1,33 @@
 /*
- * Challenge 2: Setjmp and longjmp functions
- * Author: Husain Alshaikhahmed
+  * Challenge 2: Setjmp and longjmp functions
+  * Author: Husain Alshaikhahmed
+  * Disclaimer: Using setjmp and longjmp is generally considered bad
+  * practice, similar to goto, because they can lead to hard-to-follow control
+  * flows and make the code difficult to understand and maintain. This code
+  * demonstrates the use of setjmp and longjmp; however, using this technique is
+  * considered a bad practice as it might result in 'spaghetti code'.
  * Description:
- * Date:
+   1- Create a function named errorRecover that prints out an error message and then
+      uses longjmp to transfer control back to the main function's loop.
+   2- The main function should include an infinite loop that uses setjmp at the top of
+      the loop before processing.
+   3- Add dummy code in your loop that simulates an error (by calling errorRecover) when
+      setjmp returns 0.
+   Date:
  */
 
-#include <stdio.h>
 #include <setjmp.h>
+#include <stdio.h>
+
+jmp_buf buf;
+
+// Funcation declartion
+void errorRecover();
+
+int main() {
 
 
-int main()  {
-
-
-    return 0;
+  return 0;
 }
+
+void errorRecover();
