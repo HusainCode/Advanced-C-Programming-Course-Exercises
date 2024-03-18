23117205dcaf3be9115521a8fd651884198aa839
Husain Alshaikahmed <alshaikhahmed@hotmail.com>
2024-03-18 07:03:33 -0500

    started the 3rd challenge

---
+++ Challenge 3/Char functions/char_functions.c
@@ -0,0 +1,19 @@
+/*
+  * Challenge 3: Char functions
+  * Author: Husain Alshaikhahmed
+ * Description:
+   1-Write a program to count the number of words and characters in a file OR from standard input
+   2- This program can take zero command-line arguments or one command-line argument
+       A- If there is one argument, it is interpreted as the name of a file
+       B- If there is no argument, the standard input(stdin) is to be used for input
+    Date:
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+
+    return 0;
+}
