d3c292f07c28079a5d158268e9f2d5e3837c765e
Husain Alshaikahmed <alshaikhahmed@hotmail.com>
2024-03-26 05:01:16 -0500

    words count

--- Challenge 3/Char functions/char_functions.c
+++ Challenge 3/Char functions/char_functions.c
@@ -18,50 +18,45 @@
 #include <stdlib.h>
 
 // Function declaration
-int countFileCharacters(FILE *file);
-int countInputCharacters(char userInput[]);
+// int countFileCharacters(FILE *file);
+int countInputCharacters();
 
 int main() {
 
-  char userInput[100]; // Allocate a string of 100 charcters
-  printf("Enter a prompt:");
+    //char userInput[100]; // Allocate a string of 100 charcters
+    printf("Enter a prompt:");
+    fflush(stdout); // Flush the output buffer before reading input
 
-  // Safely handle the user input with out rsiking buffer overflow
-  fgets(userInput, sizeof(userInput), stdin);
+    // Safely handle the user input with out rsiking buffer overflow
+    //fgets(userInput, sizeof(userInput), stdin);
 
- int totalWords = countInputCharacters(userInput);
-  printf("Thare are %d", totalWords);
+    int totalWords = countInputCharacters();
+    printf("There are %d words.", totalWords);
 
-  return 0;
+    return 0;
 }
 
-int countFileCharacters(FILE *file){
-   file = fopen("readCharacters.txt", "r");
-  if(file == NULL){
-    perror("Unable to opne file!");
-    return 1;
-  }
-   processFile(file); // Call function with file
+// int countFileCharacters(FILE *file){
+//    file = fopen("readCharacters.txt", "r");
+//   if(file == NULL){
+//     perror("Unable to opne file!");
+//     return 1;
+//   }
+//    processFile(file); // Call function with file
 
-  fclose(file); // Close the file
-};
+//   fclose(file); // Close the file
+// };
 
-int countInputCharacters(char userInput[]) {
-  int word = 0, count = 0;
+int countInputCharacters() {
+    int c, word = 0, count = 0;
 
-  // WRONG. NEED TO BE FIXED
-  // STOPPED HERE
-  while((userInput = getchar()) != EOF) // Read Characters til the end of file
-  {
-    if(isspace(userInput))
-    {
-      word = 0;
+    while((c = getchar()) != EOF) {
+        if(isspace(c)) {
+            word = 0;
+        } else if (!word) {
+            word = 1;
+            count++; // increment word count
+        }
     }
-    else if (!word){
-      word = 1;
-      count++;
-    }
-
-  }
-  return count;
+    return count;
 };
